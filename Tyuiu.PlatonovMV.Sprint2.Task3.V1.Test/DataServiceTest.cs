ff923a713bb6ce04bfbf254dd1ae8f42746b7cd7
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-10-05 12:36:49 +0500

    Task3

---
+++ Tyuiu.PlatonovMV.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PlatonovMV.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint2.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculateValue()
+        {
+            var ds = new DataService();
+            double x = 2;
+            double expected = Math.Round(Math.Exp(2) - 12 * 2 + Math.Cos(2), 3);
+
+            double result = ds.Calculate(x);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovMV.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tyuiu.PlatonovMV.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint2.Task3.V1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = "Спринт #2 | Выполнил: Платонов М. В. | ИСТНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Платонов Максим Владимирович | ИСТНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение функции Y в зависимости  *");
+            Console.WriteLine("* от X, используя вложенные операторы if - else. Значение X вводится      *");
+            Console.WriteLine("* с клавиатуры. Результат округлить до трёх знаков после запятой.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            var ds = new DataService();
+            double y = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"При X = {x} значение функции Y = {y}");
+
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
